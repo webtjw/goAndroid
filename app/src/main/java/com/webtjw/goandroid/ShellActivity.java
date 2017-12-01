7ee8fc0bed7f0f5320a4783cf26fc70511e4bae9
jiawei <webtjw@foxmail.com>
2017-12-01 17:13:28 +0800

    活动公共壳

---
+++ app/src/main/java/com/webtjw/goandroid/ShellActivity.java
@@ -0,0 +1,12 @@
+package com.webtjw.goandroid;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by webtjw on 2017/12/1.
+ * ShellActivity 该类是用于在活动前封装一些广泛性和功能性的类，让子类来继承
+ */
+
+public class ShellActivity extends AppCompatActivity {
+
+}
