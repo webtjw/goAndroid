0723dd3ff515af283a7a9bf39b19d0bd36ea4117
jiawei <webtjw@foxmail.com>
2017-12-01 15:25:23 +0800

    更新

--- app/src/main/java/com/webtjw/goandroid/MainActivity.java
+++ app/src/main/java/com/webtjw/goandroid/MainActivity.java
@@ -1,25 +1,97 @@
 package com.webtjw.goandroid;
 
 import android.content.Intent;
+import android.nfc.Tag;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
 import android.widget.TextView;
+
+import com.webtjw.goandroid.utils.Logcat;
 
 public class MainActivity extends AppCompatActivity {
     static final String TAG = "MainActivity";
 
-    static {
-        System.loadLibrary("native-lib");
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        makeFullscreen();
+        setContentView(R.layout.activity_main);
+
+        setResultBack();
+        setShowDialog();
     }
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        Log.i(TAG, "onCreate");
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+    protected void onDestroy() {
+        super.onDestroy();
+    }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        Logcat.i(TAG, "Result back to MainActivity!!!");
+        if (data != null) Logcat.w(TAG, "" + requestCode + " " + resultCode + " " + data.getStringExtra("returnData"));
+    }
+
+    public void setShowDialog () {
+        final AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+        builder.setIcon(R.drawable.ic_launcher_background);
+        // 设置Title的内容
+        builder.setTitle("弹出警告框");
+        // 设置Content来显示一个信息
+        builder.setMessage("确定删除吗？");
+
+        Button button = (Button) findViewById(R.id.button_dialog);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                builder.show();
+            }
+        });
+    }
+
+    // 设置本活动为全屏
+    private void makeFullscreen () {
+        // 隐藏标题栏
+        requestWindowFeature(Window.FEATURE_NO_TITLE); // 这个是 Activity 的
+        if (getSupportActionBar() != null) getSupportActionBar().hide(); // 这个是 AppCompatActivity
+        // 隐藏状态栏
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+    }
+
+    // 后面一个活动返回数据到前面一个活动
+    private void setResultBack () {
         TextView tv = (TextView) findViewById(R.id.sample_text);
         tv.setText(stringFromJNI());
 
@@ -32,46 +104,9 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        Log.i(TAG, "onDestroy");
+    // JNI
+    static {
+        System.loadLibrary("native-lib");
     }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        Log.i(TAG, "onResume");
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        Log.i(TAG, "onPause");
-    }
-
-    @Override
-    protected void onStop() {
-        super.onStop();
-        Log.i(TAG, "onStop");
-    }
-
-    @Override
-    protected void onStart() {
-        super.onStart();
-        Log.i(TAG, "onStart");
-    }
-
-    @Override
-    protected void onRestart() {
-        super.onRestart();
-        Log.i(TAG, "onRestart");
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-    }
-
     public native String stringFromJNI();
 }
---
+++ app/src/main/java/com/webtjw/goandroid/utils/Logcat.java
@@ -0,0 +1,24 @@
+package com.webtjw.goandroid.utils;
+
+import android.util.Log;
+
+// Logcat 类是为了封装处理一下 AS 自带的 logcat 不太好用的替代品
+public class Logcat {
+    private static final String TAG = "log"; // 独特的标签名，在 logcat 中与原 logcat 区分的字符串
+
+    public static void i (String tag, String info) {
+        Log.i(TAG + " " + tag, info);
+    }
+    public static void w (String tag, String info) {
+        Log.w(TAG + " " + tag, info);
+    }
+    public void e (String tag, String info) {
+        Log.e(TAG + " " + tag, info);
+    }
+    public void d (String tag, String info) {
+        Log.d(TAG + " " + tag, info);
+    }
+    public void v (String tag, String info) {
+        Log.v(TAG + " " + tag, info);
+    }
+}
