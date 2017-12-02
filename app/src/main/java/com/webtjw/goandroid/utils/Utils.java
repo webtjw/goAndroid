b512d7b6879870b77b38c6e2cc1a39c6faa67bc1
jiawei <webtjw@foxmail.com>
2017-12-03 00:07:44 +0800

    1

---
+++ app/src/main/java/com/webtjw/goandroid/utils/Utils.java
@@ -0,0 +1,16 @@
+package com.webtjw.goandroid.utils;
+
+import android.content.Context;
+import android.view.Window;
+import android.view.WindowManager;
+
+/**
+ * Created by webtjw on 2017/12/2.
+ * Utils 通用工具类
+ */
+
+public class Utils {
+    public static void hideTitle (Context context) {
+
+    }
+}
