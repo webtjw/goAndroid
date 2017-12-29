a73c5e1e668f4db31042941c4b8939ef513a4d4c
jiawei <webtjw@foxmail.com>
2017-12-29 11:52:09 +0800

    1

--- app/src/main/java/com/webtjw/goandroid/MainActivity.java
+++ app/src/main/java/com/webtjw/goandroid/MainActivity.java
@@ -26,21 +26,18 @@ public class MainActivity extends AppCompatActivity {
 
     static final String TAG = "MainActivity";
 
-    private Button button1;
-    private Button button2;
-
-    private CounterService counterService;
-    private ServiceConnection counterServiceConnect;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        makeFullscreen();
         setContentView(R.layout.activity_main);
 
-
-        initCounter();
-        initWebview();
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this, WebviewActivity.class);
+                startActivity(intent);
+            }
+        }, 2000);
     }
 
     @Override
@@ -71,90 +68,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onRestart() {
         super.onRestart();
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent intent) {
-        super.onActivityResult(requestCode, resultCode, intent);
-        if (intent != null) {
-            String age = intent.getStringExtra("msg");
-            if (age != "") {
-                Toast.makeText(this, age, Toast.LENGTH_SHORT).show();
-            }
-        }
-    }
-
-    // 可以在活动被系统回收后，保存回收前的数据，以便活动回复时，在 onCreate 里面重新读取数据
-    @Override
-    public void onSaveInstanceState(Bundle outState, PersistableBundle outPersistentState) {
-        super.onSaveInstanceState(outState, outPersistentState);
-        String jiawei = "you can save this even if the activity's killed!";
-        outState.putString("data", jiawei);
-    }
-
-    // 设置本活动为全屏
-    private void makeFullscreen() {
-        // 隐藏标题栏
-        requestWindowFeature(Window.FEATURE_NO_TITLE); // 这个是 Activity 的
-        if (getSupportActionBar() != null) getSupportActionBar().hide(); // 这个是 AppCompatActivity
-        // 隐藏状态栏
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
-    }
-
-    // 开启计数服务和子线程，同时利用 GoApplication 来 Toast
-    private void initCounter() {
-        button1 = findViewById(R.id.button1);
-        final Intent intent = new Intent(MainActivity.this, CounterService.class);
-
-        counterServiceConnect = new ServiceConnection() {
-            @Override
-            public void onServiceConnected(ComponentName componentName, IBinder iBinder) {
-                counterService = ((CounterService.CounterBinder)iBinder).getService();
-                counterService.setUpdateMethod(new UIInterface() {
-                    @Override
-                    public void updateUI(Message message) {
-                        counterHandler.sendMessage(message);
-                    }
-                });
-            }
-
-            @Override
-            public void onServiceDisconnected(ComponentName componentName) { }
-        };
-
-        button1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if (counterService != null && counterService.isBind) {
-                    unbindService(counterServiceConnect);
-                    counterServiceConnect = null;
-                } else {
-                    bindService(intent, counterServiceConnect, Context.BIND_AUTO_CREATE);
-                }
-            }
-        });
-    }
-
-    public Handler counterHandler = new Handler(new Handler.Callback() {
-        @Override
-        public boolean handleMessage(Message message) {
-            if (message.what == 0x01) {
-                int count = message.arg1;
-                Toast.makeText(GoApplication.getContext(), "当前计数：" + Integer.toString(count), Toast.LENGTH_SHORT).show();
-            }
-            return false;
-        }
-    });
-
-    private void initWebview() {
-        button2 = findViewById(R.id.button2);
-        button2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, WebviewActivity.class);
-                startActivity(intent);
-            }
-        });
     }
 
     // JNI
