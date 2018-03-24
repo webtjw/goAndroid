1d6df6211a74d672113960430ed45a03ab650be5
jiawei <webtjw@foxmail.com>
2018-03-24 09:19:40 +0800

    1

--- app/src/main/java/com/webtjw/goandroid/view/IndexActivity.java
+++ app/src/main/java/com/webtjw/goandroid/view/IndexActivity.java
@@ -4,8 +4,13 @@ import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 
 import com.webtjw.goandroid.R;
+
+import java.util.List;
 
 public class IndexActivity extends AppCompatActivity {
 
@@ -13,10 +18,24 @@ public class IndexActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_index);
+
+
     }
 
     public static void start(Context context) {
         Intent intent = new Intent(context, IndexActivity.class);
         context.startActivity(intent);
+    }
+
+    private class ItemAdapter extends ArrayAdapter<String> {
+
+        public ItemAdapter(Context context, int textViewResourceId, List<String> list) {
+            super(context, textViewResourceId, list);
+        }
+
+        @Override
+        public View getView(int i, View view, ViewGroup viewGroup) {
+            return null;
+        }
     }
 }
