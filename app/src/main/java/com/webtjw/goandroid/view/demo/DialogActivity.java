013a1e060e21c153f5f739128f7bfae189ba82f0
jiawei <webtjw@foxmail.com>
2018-04-13 17:31:32 +0800

    自定义dialog

---
+++ app/src/main/java/com/webtjw/goandroid/view/demo/DialogActivity.java
@@ -0,0 +1,15 @@
+package com.webtjw.goandroid.view.demo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.webtjw.goandroid.R;
+
+public class DialogActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dialog);
+    }
+}
